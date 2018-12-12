92133dd6401a758edfecbe05a1bb4e681a140bb1
sohail70 <sohail.e.nia@gmail.com>
2018-12-13 01:23:35 +0330

    basic template

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,12 @@
-#include<iostream>
-#include<math>
+#include <ros/ros.h>
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int a=10;
+    a++;
+    ROS_INFO_STREAM("begining of the end:");
+    ROS_INFO_STREAM(a);
+
+}
